99a2b3522927b79752a9ee81bd0bb90d103ea70c
temaxuck <gorropand@gmail.com>
2025-07-25 11:44:00 +0300

    Implement basic CLI

---
+++ cmd/caca/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/temaxuck/caca"
+	"github.com/temaxuck/caca/internal/git"
+)
+
+type Date time.Time
+type User struct {
+	author *git.GitAuthor
+}
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s [OPTIONS] <canvas file>\nOptions:\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	help := flag.Bool("h", false, "Help message")
+	verbose := flag.Bool("v", false, "Enable verbose mode")
+	preview := flag.Bool("p", false, "Enable preview mode\nWith this option enabled no commits are made")
+	repository := flag.String("repository", ".", "Target repository")
+	user := User{nil}
+	flag.Var(&user, "user", "User on behalf of whom to create commits. Format: '<name> <email>'\nIf not specified global config user setting is used")
+	startDate := Date(time.Now())
+	flag.Var(&startDate, "start-date", "Set a start date for the canvas\nYou, probably, want it to be a Sunday")
+
+	flag.Parse()
+
+	if *help {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	if flag.NArg() == 0 {
+		fmt.Println("ERROR: Specify canvas file")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	canvasFile := os.Args[len(os.Args)-flag.NArg()]
+	canvas, err := caca.ReadCanvas(canvasFile)
+	if err != nil {
+		fmt.Printf("ERROR: %v\n", err)
+		flag.Usage()
+		os.Exit(1)
+	}
+	if user.author != nil {
+		canvas.SetAuthor(user.author)
+	}
+	canvas.SetRepository(*repository)
+	startDateTime := time.Time(startDate)
+	canvas.SetStartDate(startDateTime)
+
+	fmt.Printf("INFO: Starting commits from: %v\n", startDate.String())
+
+	if *preview {
+		fmt.Printf("INFO: Canvas preview:\n%s", canvas.String())
+		os.Exit(0)
+	}
+
+	canvas.Draw(*verbose)
+}
+
+func (d *Date) String() string {
+	return time.Time(*d).Format(time.DateOnly)
+}
+
+func (d *Date) Set(value string) error {
+	t, err := time.Parse(time.DateOnly, value)
+	if err != nil {
+		return err
+	}
+
+	t = t.Add(time.Hour * 12)
+	*d = Date(t)
+	return nil
+}
+
+func (u *User) String() string {
+	if u.author != nil {
+		return "'" + u.author.Name + " " + u.author.Email + "'"
+	}
+	return "null"
+}
+
+func (u *User) Set(value string) error {
+	tokens := strings.SplitN(value, " ", 2)
+	if len(tokens) != 2 {
+		return errors.New("failed to parse user")
+	}
+
+	u.author = &git.GitAuthor{tokens[0], tokens[1]}
+	return nil
+}
